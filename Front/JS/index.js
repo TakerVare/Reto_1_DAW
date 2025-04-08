16b1ac5f2bf568be7369d98c55b9d4f43cfd6432
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2025-04-08 20:52:50 +0200

    Añado bonotes de login por js

--- Front/JS/index.js
+++ Front/JS/index.js
@@ -1,8 +1,69 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Seleccionar elementos del DOM
-    const hamburger = document.querySelector('.hamburger');
-    const navMenu = document.querySelector('.nav_menu');
-    
+    //const hamburger = document.querySelector('.hamburger');
+    //const navMenu = document.querySelector('.nav_menu');
+
+
+    //Creo esta variable para dibujar los botones de login y register 
+    //o el icono de usuario según corresponda 
+    const navUser = document.querySelector('.nav_user');
+    var usuario_estatus = localStorage.getItem("usuario");
+    localStorage.setItem("usuario", "inactivo");
+    if(navUser) {
+        if(usuario_estatus!="activo"){
+            //Creo el primer botón de Sing in
+            const newNavUser=document.getElementById('nav_user');
+            const newNavUserLogIn = document.createElement('button');
+            newNavUserLogIn.classList.add('nav_user_button');
+            newNavUserLogIn.innerText='Sing in';
+            newNavUser.appendChild(newNavUserLogIn);
+            
+            //Creo el primer botón de Register
+            const newNavUserRegister = document.createElement('button');
+            newNavUserRegister.classList.add('nav_user_button');
+            newNavUserRegister.innerText='Register';
+            newNavUser.appendChild(newNavUserRegister);
+
+            navUser = newNavUser;
+        }
+        if(usuario_estatus==="activo"){
+            
+            //Creo el avatar
+            const newNavUser=document.getElementById('nav_user');
+            const newNavUserAvatar = document.createElement('div');
+            //newNavUserLogIn.classList.add('nav_user_button');
+            newNavUserAvatar.innerHTML=`<svg id="nav_user_icon" xmlns="http://www.w3.org/2000/svg" 
+                viewBox="0 0 24 24" 
+                fill="none" 
+                stroke="currentColor" 
+                stroke-width="2" 
+                stroke-linecap="round" 
+                stroke-linejoin="round" 
+                class="lucide lucide-user-round-icon lucide-user-round">
+                <circle cx="12" cy="8" r="5"/>
+                <path d="M20 21a8 8 0 0 0-16 0"/>
+            </svg>`;
+            /*
+            newNavUserAvatar.innerText=`<svg id="nav_user_icon" xmlns="http://www.w3.org/2000/svg" 
+                viewBox="0 0 24 24" 
+                fill="none" 
+                stroke="currentColor" 
+                stroke-width="2" 
+                stroke-linecap="round" 
+                stroke-linejoin="round" 
+                class="lucide lucide-user-round-icon lucide-user-round">
+                <circle cx="12" cy="8" r="5"/>
+                <path d="M20 21a8 8 0 0 0-16 0"/>
+            </svg>`;*/
+            newNavUser.appendChild(newNavUserAvatar);
+            
+            navUser = newNavUser;
+            
+        }
+        
+
+    }
+    /*
     // Si no existe el menú hamburguesa, crearlo
     if (!hamburger) {
         const newHamburger = document.createElement('button');
@@ -52,4 +113,5 @@ document.addEventListener('DOMContentLoaded', function() {
             document.body.classList.remove('menu-open');
         }
     });
+    */
 });
